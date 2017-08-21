3110d55942ceaeefeccca3c6b0e0fa0b749c4a04
Lin Jie <ljlovemichael@hotmail.com>
2017-08-21 17:30:28 +0800

    添加项目文件。

---
+++ ContosoUniversity/Data/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        //手动指定表名
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Course>().ToTable("Course");
+        //    modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        //    modelBuilder.Entity<Student>().ToTable("Student");
+        //}
+    }
+}
